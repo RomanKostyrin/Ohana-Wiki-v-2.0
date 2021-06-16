894de2f88b9b58c733df155b1c656de3fa75fba7
roman kostyrin <enemy-iubip@mail.ru>
2021-06-16 23:23:11 +0300

    added button and input

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import Input from './Components/UI/Input/Input'
+import classes from './App.module.scss'
+import React from 'react'
+import Button from './Components/UI/Button/Button'
 
-function App() {
-  return <Input label={'Введите название'} />
+class App extends React.Component {
+  render() {
+    return (
+      <div className={classes.container}>
+        <Input
+          label={'Введите название'}
+          labelType={'InputPassword'}
+          type={'password'}
+        />
+        <Button type={'Button--important'}>Hello</Button>
+      </div>
+    )
+  }
 }
 
 export default App
